f7a1bedea6f809744b2888cb5a6ba3d89a9be7c7
Diogo Junior <diogo.junior@grad.ufsc.br>
2024-09-02 17:43:32 -0300

    Implement form for registration and use API endpoints to get/create/update user.

--- website/src/app/data.ts
+++ website/src/app/data.ts
@@ -13,6 +13,17 @@ export interface PersonResult {
     // competitionAverage: string;
 }
 
+export interface UserResponse {
+    code: number;
+    user: User;
+}
+
+export interface User {
+    wca_id: string;
+    state: string;
+    last_updated: string;
+}
+
 export enum CubingEvent {
     e333 = '333',
     e444 = '444',
@@ -20,4 +31,18 @@ export enum CubingEvent {
     e666 = '666',
     e777 = '777',
     eminx = 'minx'
+}
+
+export enum ApiCodes {
+    OK_CODE = 42,
+    USER_CREATED = 10,
+    USER_NOT_CREATED = 11,
+    USER_UPDATED = 20,
+    USER_NOT_UPDATED = 21,
+    USER_NOT_FOUND = 30
+}
+
+export interface State {
+    abbrev: string,
+    name: string
 }
--- website/src/app/services/authentication.service.ts
+++ website/src/app/services/authentication.service.ts
@@ -2,14 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   api_code: string = "";
-  token: string = "";
-  access_token_api: string = "/oauth/token/";
   data_url: string = "https://www.worldcubeassociation.org/api/v0/me"
   
   constructor(
@@ -30,9 +30,13 @@ export class AuthenticationService {
     )
   }
 
+  token(): string {
+    let tkn = sessionStorage.getItem("access_token");
+    return tkn ? tkn : "";
+  }
+
   login(): void {
     if (this.api_code == "" || this.api_code == undefined) {
-      console.log("doing login");
       let redirectUrl = `https://www.worldcubeassociation.org/oauth/authorize?client_id=OS6jVGAcxX_MwpLawxS1hRq8IVNEfu-FAthO72ARdyw&redirect_uri=${environment.APP_URL}&response_type=code&scope=public`;
       sessionStorage.setItem("is_getting_token", "true");
       window.location.href = redirectUrl;
@@ -40,30 +44,45 @@ export class AuthenticationService {
   }
 
   requestToken(): void {
-    // let cid = wcaSecrets.client_id;
-    // let sec = wcaSecrets.secret;
-    let cid = "OS6jVGAcxX_MwpLawxS1hRq8IVNEfu-FAthO72ARdyw";
-    let sec = "OP_J3qaVdOVL0I5vTwzKwRsyY2EVq9xZRKM9KsofN1I";
     let api_params = {
-      grant_type:'authorization_code',
-      code:this.api_code,
-      client_id:cid,
-      client_secret:sec,
       redirect_uri:window.location.origin
     }
     console.log(api_params);
-    this.httpClient.post<any>(this.access_token_api, api_params).subscribe(
+    this.httpClient.post<any>(`${environment.BACKEND_URL}/token/${this.api_code}`, api_params).subscribe(
       (response: any) => {
-        this.token = response['access_token'];
-        console.log(`Got token ${this.token}`);
+        // TODO: check if there is no token
+        sessionStorage.setItem("access_token", response['access_token']);
+        console.log(`Got token ${this.token()}`);
         this.getUserWID();
       }
     )
   }
 
+  getUserData(wca_id: string): Observable<any> {
+    return this.httpClient.get<any>(`${environment.BACKEND_URL}/user/${wca_id}`)
+  }
+
+  createUser(userData: User): Observable<any> {
+    let body = {
+      state: userData.state,
+      access_token: this.token(),
+    };
+    return this.httpClient.post<any>(`${environment.BACKEND_URL}/user/${userData.wca_id}`, body);
+  }
+
+  updateUser(userData: User): Observable<any> {
+    let body = {
+      state: userData.state,
+      access_token: this.token(),
+    };
+    console.log("Putting");
+    console.log(body);
+    return this.httpClient.put<any>(`${environment.BACKEND_URL}/user/${userData.wca_id}`, body);
+  }
+
   getUserWID(): void {
     let headers = {
-      'Authorization': `Bearer ${this.token}`,
+      'Authorization': `Bearer ${this.token()}`,
       'Content-Type': 'application/json'
     }
     this.httpClient.get<any>(this.data_url, {headers: headers}).subscribe(
